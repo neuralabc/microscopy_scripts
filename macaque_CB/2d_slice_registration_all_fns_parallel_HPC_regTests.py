08f14d082014c507545146cffd1fed9bab9b32db
steelec <steelec@gmail.com>
2024-11-04 14:49:14 -0500

    updated outputs to log

--- macaque_CB/2d_slice_registration_all_fns_parallel_HPC_regTests.py
+++ macaque_CB/2d_slice_registration_all_fns_parallel_HPC_regTests.py
@@ -171,7 +171,7 @@ def coreg_single_slice_orig(idx, output_dir, subject, img, all_image_names, temp
             image_weights_ordered.append(image_weights[idx2 + 1])
             
     
-    logging.info(f'slice_idx: {idx}\nsources: {sources[0].split("/")[-1]}\ntargets:\n{[t.split("/")[-1] for t in targets]}\nweights:\n{image_weights_ordered}') #source is always the same 
+    logging.info(f'\n\tslice_idx: {idx}\n\t\tsources: {sources[0].split("/")[-1]}\n\t\ttargets: {[t.split("/")[-1] for t in targets]}\n\t\tweights: {image_weights_ordered}') #source is always the same 
 
     # logging.info('Sources:')
     # for s in sources:

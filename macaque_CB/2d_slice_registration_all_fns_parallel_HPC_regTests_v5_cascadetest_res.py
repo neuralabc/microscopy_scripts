742671a19b47a2f05c64a285cd553de80077bc0e
steelec <steelec@gmail.com>
2024-12-18 10:01:33 -0500

    moved over to nighres registrations for cascade, better results so far

--- macaque_CB/2d_slice_registration_all_fns_parallel_HPC_regTests_v5_cascadetest_res.py
+++ macaque_CB/2d_slice_registration_all_fns_parallel_HPC_regTests_v5_cascadetest_res.py
@@ -51,14 +51,17 @@ mask_zero = True #mask zeros for nighres registrations
 
 # rescale=5 #larger scale means that you have to change the scaling_factor
 # scaling_factor = 64 #32 or 64 for full scaling of resolutions on the registrations
-rescale=40
+rescale=20
 scaling_factor=16
 #based on the rescale value, we adjust our in-plane resolution
 in_plane_res_x = rescale*in_plane_res_x/1000
 in_plane_res_y = rescale*in_plane_res_y/1000
 in_plane_res_z = in_plane_res_z/1000
 
-voxel_res = [in_plane_res_x,in_plane_res_y,in_plane_res_z]
+actual_voxel_res = [in_plane_res_x,in_plane_res_y,in_plane_res_z]
+#if we don't want to set the voxel resolution, we can set it to None and it will be 1x1x1
+# voxel_res = None # registration performs much better when we do not specify the res
+voxel_res = actual_voxel_res
 
 downsample_parallel = False #True means that we invoke Parallel, but can be much faster when set to False since it skips the Parallel overhead
 max_workers = 100 #number of parallel workers to run for registration -> registration is slow but not CPU bound on an HPC (192 cores could take ??)
@@ -68,18 +71,18 @@ nonlin_interp_max_workers = 100 #number of workers to use for nonlinear slice in
 
 
 
-output_dir = f'/tmp/slice_reg_perSliceTemplate_image_weights_dwnsmple_parallel_v2_{rescale}_casc_v5_test_v2/'
+output_dir = f'/tmp/slice_reg_perSliceTemplate_image_weights_dwnsmple_parallel_v2_{rescale}_casc_v5_test_v3/'
 # _df = pd.read_csv('/data/neuralabc/neuralabc_volunteers/macaque/all_TP_image_idxs_file_lookup.csv')
 missing_idxs_to_fill = [32,59,120,160,189,228] #these are the slice indices with missing or terrible data, fill with mean of neigbours
 # output_dir = '/data/data_drive/Macaque_CB/processing/results_from_cell_counts/slice_reg_perSliceTemplate_image_weights_all_tmp/'
 _df = pd.read_csv('/data/data_drive/Macaque_CB/processing/results_from_cell_counts/all_TP_image_idxs_file_lookup.csv')
 
 # missing_idxs_to_fill = [5,32]
-# missing_idxs_to_fill = [3]
+missing_idxs_to_fill = [5]
 # missing_idxs_to_fill = None
 all_image_fnames = list(_df['file_name'].values)
 
-# all_image_fnames = all_image_fnames[0:50] #for testing
+all_image_fnames = all_image_fnames[155:165] #for testing
 
 print('*********************************************************************************************************')
 print(f'Output directory: {output_dir}')
@@ -447,6 +450,84 @@ def run_cascading_coregistrations(output_dir, subject, all_image_fnames, anchor_
             ants.image_write(pre_aligned, output) #if we are not running the nonlin, just write the rigidly aligned image
         logging.warning(f"\t\tCascade registration completed for slice {src_idxs[idx]}.")
 
+def run_cascading_coregistrations_v2(output_dir, subject, all_image_fnames, anchor_slice_idx = None,
+                                  missing_idxs_to_fill = None, zfill_num=4, input_source_file_tag='coreg0nl', 
+                                  reg_level_tag='coreg1nl', previous_target_tag=None, run_syn=True, scaling_factor=64,
+                                  mask_zero=mask_zero):
+  
+    # params for regularization of nonlinear deformations w/ 'SyNOnly'
+        # from nighres, the last two numbers of the syn_param correspond to the flow and total sigmas (fluid and elastic deformations, respectively)
+        # if regularization == 'Low': syn_param = [0.2, 1.0, 0.0]
+        # elif regularization == 'Medium': syn_param = [0.2, 3.0, 0.0]
+        # elif regularization == 'High': syn_param = [0.2, 4.0, 3.0]
+    syn_flow_sigma = 4.0 #3 is the default w/ this ants.registration call
+    syn_total_sigma = 3.0 #0 is the default w/ this ants.registration call
+
+    import ants
+
+    if previous_target_tag is not None:
+        previous_tail = f'_{previous_target_tag}_ants-def0.nii.gz' #if we want to use the previous iteration rather than building from scratch every time (useful for windowing)
+    else:
+        previous_tail = f'_{input_source_file_tag}_ants-def0.nii.gz'
+
+    #identify a central slice to start our registration from, rather than anchoring @ the end
+    #but we make sure that it is not a missing slice
+    
+    if anchor_slice_idx is None:
+        anchor_slice_idx = int(numpy.floor(len(all_image_fnames)/2))
+
+    if missing_idxs_to_fill is not None:
+        valid_slices = set(range(len(all_image_fnames))) - set(missing_idxs_to_fill)
+        closest_slice = min(valid_slices, key=lambda x: abs(x - anchor_slice_idx), default=None)
+        if closest_slice is None:
+            raise ValueError("No valid start slice index found in the stack.")
+        anchor_slice_idx = closest_slice
+    
+    # list of what our outputs will be 
+    all_image_fnames_new = []
+    for idx in numpy.arange(len(all_image_fnames)):
+        img_basename = os.path.basename(all_image_fnames[idx]).split('.')[0]
+        all_image_fnames_new.append(f"{output_dir}{subject}_{str(idx).zfill(zfill_num)}_{img_basename}_{reg_level_tag}_ants-def0.nii.gz")
+
+    #list of what our .nii inputs should be
+    all_image_fnames_nii = []
+    for idx in numpy.arange(len(all_image_fnames)):
+        img_basename = os.path.basename(all_image_fnames[idx]).split('.')[0]
+        all_image_fnames_nii.append(f"{output_dir}{subject}_{str(idx).zfill(zfill_num)}_{img_basename}{previous_tail}")
+
+    #load and then save the central slice with the new tag, no change since this is the space we want to align to
+    save_volume(all_image_fnames_new[anchor_slice_idx],load_volume(all_image_fnames_nii[anchor_slice_idx]))
+
+    #define leftware and rightward indices, then split to source and targets so that we register 4<-5, 5<-6, ... and 3->4, 2->3, ... 
+    rw_idxs = numpy.arange(anchor_slice_idx,len(all_image_fnames_nii))
+    lw_idxs = numpy.arange(anchor_slice_idx,-1,-1)
+
+    # this is setup to register adjacent slices to that central slice, then cascade the registrations to the left and right
+    rw_src_idxs = rw_idxs[1:]
+    rw_trg_idxs = rw_idxs[:-1]
+    lw_src_idxs = lw_idxs[1:]
+    lw_trg_idxs = lw_idxs[:-1]
+
+    src_idxs = numpy.concatenate((rw_src_idxs,lw_src_idxs))
+    trg_idxs = numpy.concatenate((rw_trg_idxs,lw_trg_idxs))
+
+
+    #run the registrations
+    for idx, _  in enumerate(src_idxs):
+        # img_basename = os.path.basename(all_image_fnames[idx]).split('.')[0]
+        target_idx = trg_idxs[idx]
+        moving_idx = src_idxs[idx]
+
+        # in each case, only one source and one target, but we use the same code as above
+        source = all_image_fnames_nii[moving_idx]
+        target = all_image_fnames_new[target_idx] #targets always come from the new list, since this is where the registrered sources will be (and we pre-filled the start_slice_idx image)
+        output = all_image_fnames_new[moving_idx]
+
+        reg_aligned = do_reg([source], [target], file_name=output, output_dir=output_dir, run_syn=run_syn, 
+                             scaling_factor=scaling_factor,mask_zero=mask_zero)
+        save_volume(output, load_volume(reg_aligned['transformed_source']) ,overwrite_file=True)
+        logging.warning(f"\t\tCascade registration version 2 completed for slice {src_idxs[idx]}.")
+
 def compute_intermediate_non_linear_slice(pre_img, post_img, current_img=None, additional_coreg_mean = True, 
                                           idx=None):
     """
@@ -712,6 +793,11 @@ def compute_intermediate_slice(pre_img, post_img, current_img=None, idx=None, de
             img2 = load_volume(post_avg['transformed_source'])
 
             avg = (img1.get_fdata() + img2.get_fdata()) / 2
+            
+            #on the last iteration, we do not compute the average but rather the first image in the average space to reduce overlap errors
+            if (current_img is None) and (refinement_iter == reg_refinement_iterations - 1):
+                avg = img1.get_fdata()
+
             avg = nibabel.Nifti1Image(avg, affine=img1.affine, header=img1.header, dtype=img1.get_data_dtype())
             save_volume(avg_fname, avg, overwrite_file=True)
 
@@ -1734,11 +1820,12 @@ for iter in range(num_cascade_iterations):
     else:
         input_source_file_tag = iter_tag #updates with the previous iteration
     iter_tag = f'cascade_{iter}'
-    run_cascading_coregistrations(output_dir, subject, 
+    run_cascading_coregistrations_v2(output_dir, subject, 
                                 all_image_fnames, anchor_slice_idx = anchor_slice_idxs[iter], 
                                 missing_idxs_to_fill = missing_idxs_to_fill, 
                                 zfill_num=zfill_num, input_source_file_tag=input_source_file_tag, 
-                                reg_level_tag=iter_tag, previous_target_tag=None, run_syn=True)
+                                reg_level_tag=iter_tag, previous_target_tag=None, run_syn=True,
+                                scaling_factor=scaling_factor,mask_zero=mask_zero)
 
     template = generate_stack_and_template(output_dir,subject,all_image_fnames,zfill_num=zfill_num,reg_level_tag=iter_tag,
                                         per_slice_template=True,missing_idxs_to_fill=missing_idxs_to_fill,

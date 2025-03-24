e89a9ac4f9077bf37584c1ee55ffca8bf9c6ba29
steelec <steelec@gmail.com>
2025-03-24 17:27:56 -0400

    moved cascade reg to do_reg (from ants)

--- macaque_CB/2d_slice_registration_all_fns_parallel_HPC_regTests_v5_cascadetest_res.py
+++ macaque_CB/2d_slice_registration_all_fns_parallel_HPC_regTests_v5_cascadetest_res.py
@@ -649,7 +649,7 @@ def run_cascading_coregistrations_v2(output_dir, subject, all_image_fnames, anch
             target = all_image_fnames_new[target_idx] #targets always come from the new list, since this is where the registrered sources will be (and we pre-filled the start_slice_idx image)
             output = all_image_fnames_new[moving_idx]
             # previously was just do_reg()
-            reg_aligned = do_reg_ants([source], [target], file_name=output, output_dir=temp_out_dir, run_syn=run_syn, 
+            reg_aligned = do_reg([source], [target], file_name=output, output_dir=temp_out_dir, run_syn=run_syn, 
                                 scaling_factor=scaling_factor,mask_zero=mask_zero)
             save_volume(output, load_volume(reg_aligned['transformed_source']) ,overwrite_file=True)
             logging.warning(f"\t\tCascade registration version 2 completed for slice {src_idxs[idx]}.")

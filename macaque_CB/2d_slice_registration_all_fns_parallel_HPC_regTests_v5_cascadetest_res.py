ea8c9a59961faa02605b22e7459e318757597e22
steelec <steelec@gmail.com>
2025-03-26 15:10:07 -0400

    mattes works well, full test

--- macaque_CB/2d_slice_registration_all_fns_parallel_HPC_regTests_v5_cascadetest_res.py
+++ macaque_CB/2d_slice_registration_all_fns_parallel_HPC_regTests_v5_cascadetest_res.py
@@ -72,18 +72,18 @@ nonlin_interp_max_workers = 50 #number of workers to use for nonlinear slice int
 
 
 
-output_dir = f'/tmp/slice_reg_perSliceTemplate_image_weights_dwnsmple_parallel_v2_{rescale}_casc_v5_test_v4_part_med/'
+output_dir = f'/tmp/slice_reg_perSliceTemplate_image_weights_dwnsmple_parallel_v2_{rescale}_casc_v5_test_v4_full_med_mattes/'
 _df = pd.read_csv('/data/neuralabc/neuralabc_volunteers/macaque/all_TP_image_idxs_file_lookup.csv')
-# missing_idxs_to_fill = [32,59,120,160,189,228] #these are the slice indices with missing or terrible data, fill with mean of neigbours
+missing_idxs_to_fill = [32,59,120,160,189,228] #these are the slice indices with missing or terrible data, fill with mean of neigbours
 # output_dir = '/data/data_drive/Macaque_CB/processing/results_from_cell_counts/slice_reg_perSliceTemplate_image_weights_all_tmp/'
 ## _df = pd.read_csv('/data/data_drive/Macaque_CB/processing/results_from_cell_counts/all_TP_image_idxs_file_lookup.csv')
 
-missing_idxs_to_fill = [32]
+#missing_idxs_to_fill = [32]
 # missing_idxs_to_fill = [5]
 # missing_idxs_to_fill = None
 all_image_fnames = list(_df['file_name'].values)
 
-all_image_fnames = all_image_fnames[0:35] #for testing
+#all_image_fnames = all_image_fnames[0:35] #for testing
 
 print('*********************************************************************************************************')
 print(f'Output directory: {output_dir}')

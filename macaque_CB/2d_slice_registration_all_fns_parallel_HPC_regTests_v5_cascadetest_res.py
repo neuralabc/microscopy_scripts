dfd22c777adb43635a5c72f97f31dd6f4b831ea4
steelec <steelec@gmail.com>
2025-03-26 11:11:25 -0400

    fox

--- macaque_CB/2d_slice_registration_all_fns_parallel_HPC_regTests_v5_cascadetest_res.py
+++ macaque_CB/2d_slice_registration_all_fns_parallel_HPC_regTests_v5_cascadetest_res.py
@@ -83,7 +83,7 @@ missing_idxs_to_fill = [32]
 # missing_idxs_to_fill = None
 all_image_fnames = list(_df['file_name'].values)
 
-# all_image_fnames = all_image_fnames[0:35] #for testing
+all_image_fnames = all_image_fnames[0:35] #for testing
 
 print('*********************************************************************************************************')
 print(f'Output directory: {output_dir}')

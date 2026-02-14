5ee2aac2e564991244524c3d656e881fe0ee6116
steelec <steelec@gmail.com>
2026-02-14 11:48:37 -0500

    added subset for testing

--- macaque_CB/run_slice_registration.py
+++ macaque_CB/run_slice_registration.py
@@ -64,7 +64,9 @@ missing_idxs_to_fill = [32,59,120,160,189,228] #these are the slice indices with
 # missing_idxs_to_fill = None
 all_image_fnames = list(_df['file_name'].values)
 
-#all_image_fnames = all_image_fnames[0:35] #for testing
+## for testing XXX
+all_image_fnames = all_image_fnames[0:35] #for testing
+missing_idxs_to_fill = missing_idxs_to_fill[0]
 
 print('*********************************************************************************************************')
 print(f'Output directory: {output_dir}')

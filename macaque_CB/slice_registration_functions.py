c05091d14823d46d408154738cf3711e8e647f4b
steelec <steelec@gmail.com>
2026-02-14 11:53:26 -0500

    smaller stack

--- macaque_CB/slice_registration_functions.py
+++ macaque_CB/slice_registration_functions.py
@@ -1900,12 +1900,12 @@ def compute_MI_for_slice(idx, img_name, output_dir, subject, template_tail, out_
         shutil.copyfile(slice2_path, output_path)
     
     # Clean up old files
-    logging.warning(f'Removing old intermediate files for idx {idx}: {img_name}')
+    # logging.warning(f'Removing old intermediate files for idx {idx}: {img_name}')
     for f in glob.glob(output_dir + subject + f'_{str(idx).zfill(zfill_num)}_' + img_name + "*_ants-*map.nii.gz"):
         if out_tail not in f:
             os.remove(f)
             time.sleep(0.5)
-    logging.warning(f'---> Intermediate files removed')
+    # logging.warning(f'---> Intermediate files removed')
 
     os.remove(slice1_path)
     time.sleep(0.5)
@@ -1936,7 +1936,7 @@ def select_best_reg_by_MI_parallel(output_dir, subject, all_image_fnames, df_str
         (idx, img_name, output_dir, subject, template_tail, out_tail, tag1_tail, tag2_tail, zfill_num, per_slice_template, overwrite)
         for idx, img_name in enumerate(all_image_fnames)
     ]
-    logging.warning(all_image_fnames)    
+    # logging.warning(all_image_fnames)    
     with ProcessPoolExecutor(max_workers=max_workers) as executor:
         future_to_img = {executor.submit(compute_MI_for_slice, *arg): arg[1] for arg in args}
         for future in as_completed(future_to_img):
